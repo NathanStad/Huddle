4b649b3db5286196ec0b5b4c40d3a7e460324eeb
Michael <michael.cheneval@logixcube.ch>
2025-04-07 11:10:43 +0200

    Test Infomaniak FTP

--- src/layouts/Title.vue
+++ src/layouts/Title.vue
@@ -17,7 +17,7 @@ const props = defineProps({
 
 <template>
   <div class="p-6 w-full flex flex-col items-center text-center text-green gap-y-5 mt-8">
-    <img v-if="logo" class="logo" src="../assets/img/logo.jpg" :alt="title" />
+    <img v-if="logo" class="logo" :src="logo" :alt="title" />
     <h1 v-else class="text-6xl">{{ title }}</h1>
     <h2 v-if="subtitle" class="text-2xl">{{ subtitle }}</h2>
     <!-- <button class="bg-green text-white hover:bg-yellow hover:text-black transition rounded-lg px-5 py-3">Bouton</button> -->
